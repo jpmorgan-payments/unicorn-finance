f93fcbce8000537a02916bd9be7c28ededf09d33
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-01-20 15:27:28 +0000

    Adding an overall boundary

--- app/client/src/components/layout.tsx
+++ app/client/src/components/layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import { AppContext } from '../context/AppContext';
 import JsonDialog from './JsonDialog';
 import Sidebar from './sidebar';
 import WhatAPI from './whatApi';
+import ErrorFallback from './errorFallback';
 
 function Layout() {
   const {
@@ -22,14 +24,19 @@ function Layout() {
     <div className="flex flex-col lg:flex-row h-full text-gray-900 min-h-screen w-full max-w-screen">
       <Sidebar />
       <main className="h-auto lg:h-full lg:min-h-screen lg:w-11/12 w-full">
-        <Outlet />
-        <JsonDialog />
-        <WhatAPI
-          toggleMockedData={toggleMockedData}
-          mockedDataEnabled={displayingMockedData}
-          toggleApiData={toggleApiData}
-          apiDataEnabled={displayingApiData}
-        />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+        >
+          <Outlet />
+          <JsonDialog />
+          <WhatAPI
+            toggleMockedData={toggleMockedData}
+            mockedDataEnabled={displayingMockedData}
+            toggleApiData={toggleApiData}
+            apiDataEnabled={displayingApiData}
+          />
+        </ErrorBoundary>
+
       </main>
     </div>
   );
