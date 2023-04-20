408229e045d7dc27496701e7df2ee6aa93ed3ebf
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2023-04-20 08:48:36 +0100

    Simplifying WhatAPI

--- app/client/src/components/layout.tsx
+++ app/client/src/components/layout.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
-import { AppContext } from '../context/AppContext';
 import JsonDialog from './json_dialog';
 import Sidebar from './sidebar';
 import WhatAPI from './what_api';
 import ErrorFallback from './error_fallback';
 
 function Layout() {
-  const {
-    displayingMockedData,
-    setDisplayingMockedData,
-    displayingApiData,
-    setDisplayingApiData,
-  } = React.useContext(AppContext);
-
-  const toggleMockedData = () => {
-    setDisplayingMockedData(!displayingMockedData);
-  };
-  const toggleApiData = () => {
-    setDisplayingApiData(!displayingApiData);
-  }; return (
+  return (
     <div className="flex flex-col lg:flex-row h-full text-gray-900 min-h-screen w-full max-w-screen">
       <Sidebar />
       <main className="h-auto lg:h-full lg:min-h-screen lg:w-11/12 w-full">
@@ -29,12 +16,7 @@ function Layout() {
         >
           <Outlet />
           <JsonDialog />
-          <WhatAPI
-            toggleMockedData={toggleMockedData}
-            mockedDataEnabled={displayingMockedData}
-            toggleApiData={toggleApiData}
-            apiDataEnabled={displayingApiData}
-          />
+          <WhatAPI />
         </ErrorBoundary>
 
       </main>
