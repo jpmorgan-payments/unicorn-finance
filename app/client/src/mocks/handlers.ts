831846680e22865cd40d78e096a56313250820a1
Maria McParland <78078583+maria-mcparland@users.noreply.github.com>
2025-09-05 13:42:17 +0100

    Mocked responses

--- app/client/src/mocks/handlers.ts
+++ app/client/src/mocks/handlers.ts
@@ -2,7 +2,7 @@ import { HttpResponse, http } from "msw";
 import accountBalanceMockedResponse from "./mockedJson/AccountBalances.json";
 import validationServicesACSResponse from "./mockedJson/ValidationServicesACS.json";
 import validationServicesAuthResponse from "./mockedJson/ValidationServicesAuth.json";
-
+import globalPaymentsResponse from "./mockedJson/GlobalPayments.json";
 const errorResponse = {
   errors: [
     {
@@ -10,19 +10,6 @@ const errorResponse = {
       errorMsg: "System is Unavailable",
     },
   ],
-};
-
-const paymentInitiationResponse = {
-  paymentId: "12345678-1234-1234-1234-123456789012",
-  status: "ACCEPTED",
-  paymentSubmissionId: "PMT-2023-001",
-  creationDateTime: new Date().toISOString(),
-  statusUpdateDateTime: new Date().toISOString(),
-  data: {
-    paymentId: "12345678-1234-1234-1234-123456789012",
-    status: "ACCEPTED",
-    creationDateTime: new Date().toISOString(),
-  },
 };
 
 // Define handlers that catch the corresponding requests and returns the mock data.
@@ -38,14 +25,33 @@ export const handlers = [
       { status: 200 },
     );
   }),
-  http.post("/api/digitalSignature/payment/v2/payments", ({ request }) => {
-    const url = new URL(request.url);
-    const statusCode = url.searchParams.get("statusCode");
-    if (statusCode === "401") {
-      return new HttpResponse(JSON.stringify(errorResponse), { status: 500 });
-    }
-    return HttpResponse.json({ paymentInitiationResponse }, { status: 200 });
-  }),
+  http.post(
+    "/api/digitalSignature/payment/v2/payments",
+    async ({ request }) => {
+      const url = new URL(request.url);
+      const statusCode = url.searchParams.get("statusCode");
+      if (statusCode === "401") {
+        return new HttpResponse(JSON.stringify(errorResponse), { status: 500 });
+      }
+
+      // Parse request body to get requestId
+      let endToEndId = "default-endToEnd-id";
+      let response: { endToEndId?: string; paymentId?: string } | null = {};
+      const requestBody = (await request.json()) as any[];
+      console.log("Request Body:", requestBody);
+      if (requestBody && requestBody.length > 0) {
+        endToEndId = requestBody[0].endToEndId || "id";
+      }
+
+      if (response) {
+        response.endToEndId = endToEndId || "default-endToEnd-id";
+        response.paymentId = crypto.randomUUID();
+        return HttpResponse.json({ response }, { status: 200 });
+      } else {
+        return new HttpResponse(JSON.stringify(errorResponse), { status: 500 });
+      }
+    },
+  ),
   http.post("/api/tsapi/v2/validations/accounts", async ({ request }) => {
     const url = new URL(request.url);
     const statusCode = url.searchParams.get("statusCode");
